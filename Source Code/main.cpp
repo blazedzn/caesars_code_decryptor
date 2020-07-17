e7f73be4783f4ebdfbca956dcd0930582a113b55
blazedzn <54804805+blazedzn@users.noreply.github.com>
2020-07-17 20:53:09 +0300

    Переход с c-style строк в c++-style строки
    Переход с c-style строк в c++-style строки | Switch from c-style strings to c++-style strings

--- Source Code/main.cpp
+++ Source Code/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <cstring>
 using namespace std;
 
 int main(){
     //Переменные
-    char e_message[1000]{};
-    char d_message[1000]{};
+    string e_message{};
+    string d_message{};
     vector <int> e_message_ascii{};
     vector <int> d_message_ascii{};
     char selection{' '};
@@ -23,17 +24,17 @@ int main(){
         //Шифрование
         if(selection == 'e' or selection == 'E') {
             cout << "Enter message you want to encrypt: ";
-            cin.getline(e_message,100);
-            cin.getline(e_message,100);
+            getline(cin, e_message);
+            getline(cin, e_message);
             cout << "Enter decrypt size: ";
             cin >> decrypt_size;
             cout << "Your message: " << e_message << endl;
             //шифруем сообщение (переводим символы в ascii формат)
-            for (int i {0} ; i < strlen(e_message) ; i++){
+            for (int i {0} ; i < e_message.length() ; i++){
                 e_message_ascii.push_back(e_message[i]);
             }
             //шифруем сообщение (добавляем ascii символам +1 к их значению)
-            for (int i {0} ; i < strlen(e_message) ; i++) {
+            for (int i {0} ; i < e_message.length() ; i++) {
                 e_message_ascii[i] += decrypt_size;
             }
             //Выводим зашифрованное сообщение
@@ -48,17 +49,17 @@ int main(){
 
         else if(selection == 'D' or selection == 'd'){
             cout << "Enter a message you want to decrypt: ";
-            cin.getline(d_message,100);
-            cin.getline(d_message,100);
+            getline(cin, d_message);
+            getline(cin, d_message);
             cout << "Enter decrypt size: ";
             cin >> decrypt_size;
             cout << "Your message: " << d_message << endl;
             //расшифровываем  сообщение (переводим символы в ascii формат)
-            for (int i {0} ; i < strlen(d_message) ; i++){
+            for (int i {0} ; i < d_message.length() ; i++){
                 d_message_ascii.push_back(d_message[i]);
             }
             //расшифровываем сообщение (добавляем ascii символам -1 к их значению)
-            for (int i {0} ; i < strlen(d_message) ; i++) {
+            for (int i {0} ; i < d_message.length() ; i++) {
                 d_message_ascii[i] -= decrypt_size;
             }
             //Выводим зашифрованное сообщение
